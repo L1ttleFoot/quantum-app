1e0b5ae04611e9f9a56bc7698acee2751fc9718f
L1ttleFoot <leonid.klinck@yandex.ru>
2022-08-12 17:42:45 +0800

    добавил фласк, добавил redux

--- react-app/src/common/index.js
+++ react-app/src/common/index.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import './style.css'
-import {TextField, Paper, Stack } from '@mui/material'
+import {TextField, Paper, Stack, Button } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid';
 import Chart from "../components/Charts"
+import fetchData from "./action"
+//import Field from "../components/ConstantsFields"
 
-export const Page = () => {
+const Page = () => {
+
+    const dispatch = useDispatch()
 
     const columns = [
         {
@@ -36,23 +41,38 @@ export const Page = () => {
         { id: 6, transition: '0 => 5', energy: 11545.5395, matrix: 0 },    
       ];
 
-    const [value1,setValue1] = useState([{value:2309, index:'1'}])
+    const [value1,setValue1] = useState([{value:2309,index:'1'}])
     const [value2,setValue2] = useState([{value:-0.095,index:'1,1,1'},{value:0.0103,index:'1,1,1,1'},{value:0,index:'1,1,1,1,1'}])
     const [value3,setValue3] = useState([{value:0.445,index:'0'},{value:-0.00552,index:'1'},{value:0.00568,index:'1,1'},{value:-0.00505,index:'1,1,1'}])
 
     const [selectedRows,setSelectedRows] = useState([])
 
+    const setValue_1 = (value) => {
+        dispatch({type:'SET_VALUE1', payload:value})
+    }
+
+    const state = useSelector(state=>state)
+
+    console.log(state)
+
+    const handleClick = () => {
+        console.log('123')
+        fetch(`http://localhost:8080/api/v1/json?value=${value1[0].value}`)
+        .then(response=>response.json())
+        .then(data=>console.log(data))
+    };
+
     const handleChange1 = index => event => {
         let newArr=[...value1]
         newArr[index].value=event.target.value
         setValue1(newArr);
+        setValue_1(newArr)
     };
 
     const handleChange2 = index => event => {
         let newArr=[...value2]
         newArr[index].value=event.target.value
         setValue2(newArr);
-        console.log(value2)
     };
 
     const handleChange3 = index => event => {
@@ -62,35 +82,43 @@ export const Page = () => {
     };
 
     return(
-        <div className='container'>
-            <Paper className={'box1'}>
-                <Stack direction={'column'} alignItems='center'>
+        <div className={'container'}>
+
+            {/* <Field class={'box1'} label={'omega'} /> */}
+
+            <Paper className='box1'>
+                <Stack direction={'column'} alignItems={'center'}>
                     {value1.map((item,index)=>
-                        <TextField key={item.index+'-omega'} size='small' style={{margin:15}} label={decodeURI('%CF%89')+item.index} value={item.value} onChange={handleChange1(index)}/>
+                        <TextField key={item.index +'-omega'} size={'small'} style={{margin:15}} label={decodeURI('%CF%89')+item.index} value={item.value} onChange={handleChange1(index)}/>
                     )}
                 </Stack>
+
+                <Button onClick={handleClick}>
+                    Test
+                </Button>
+
             </Paper>
 
             <Paper className={'box2'}>
-                <Stack direction={'column'} alignItems='center'>
+                <Stack direction='column' alignItems={'center'}>
                     {value2.map((item,index) => 
-                        <TextField key={item.index+'-k'} size='small' style={{margin:15}} label={'k '+item.index} value={item.value} onChange={handleChange2(index)}/>
+                        <TextField key={item.index +'-k'} size={'small'} style={{margin:15}} label={'k '+item.index} value={item.value} onChange={handleChange2(index)}/>
                     )}
                 </Stack>
             </Paper>
 
             <Paper className={'box3'}>
-                <Stack direction={'column'} alignItems='center'>
+                <Stack direction={'column'} alignItems={'center'}>
                     {value3.map((item,index) => 
-                        <TextField key={item.index+'-d'} size='small' style={{margin:15}} label={'d '+item.index} value={item.value} onChange={handleChange3(index)}/>
+                        <TextField key={item.index +'-d'} size={'small'} style={{margin:15}} label={'d '+item.index} value={item.value} onChange={handleChange3(index)}/>
                     )}
                 </Stack>
             </Paper>
 
             <Paper className={'box4'}>
-                <div style={{ height: '100%', width: '100%' }}>
+                <div >
                     <DataGrid
-                    autoHeight
+                        autoHeight
                         rows={rows}
                         columns={columns}
                         checkboxSelection
@@ -101,7 +129,7 @@ export const Page = () => {
                 </div>
             </Paper>
 
-            <Paper className={'box6'}>
+            <Paper className={'box5'}>
                 <Chart data={selectedRows}/>
             </Paper>
         </div>
