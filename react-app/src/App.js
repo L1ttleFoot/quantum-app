4f752b8289af2c3f2bea89592189c37b3d51e6f5
L1ttleFoot <leonid.klinck@yandex.ru>
2022-08-23 21:10:56 +0800

    Revert "Revert "добавил фласк, добавил redux""
    This reverts commit 3e4d3a706dc4d3f8fdd271cf44b5ee10feeb4859.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,27 @@
+/* import { useDispatch, useSelector } from 'react-redux'; */
 import './App.css';
 import Page from './common'
 
 function App() {
+
+  /* const dispatch = useDispatch()
+
+  const cash = useSelector(state => state.cash) */
+
+  /* const addCash = () => {
+    dispatch({type:'ADD_CASH', payload:5})
+  }
+
+  const getCash = () => {
+    dispatch({type:'GET_CASH', payload:5})
+  } */
+
   return (
     <div className="App">
       <Page/>
+      {/* <div>{cash}</div>
+       <button onClick={()=> addCash()}>+ cach</button>
+      <button onClick={()=> getCash()}>- cach</button> */}
     </div>
   );
 }
