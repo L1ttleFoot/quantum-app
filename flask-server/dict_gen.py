c18bf99c878184010b67d7556d1120ad50d9a8e5
L1ttleFoot <leonid.klinck@yandex.ru>
2022-12-04 00:15:01 +0800

    try to some calculation fixes

--- flask-server/dict_gen.py
+++ flask-server/dict_gen.py
@@ -12,7 +12,7 @@ def INDEX(n:int, number_of_vibrational_degrees)->list:
     ''' Индексы вектора vec, степени возмущения n, vec дб tuple для хэширования '''
     if n==-2:return [('0')] # добавил так как в VECTOR_INDEX добавил 0
     
-    return [''.join(i) for i in combinations_with_replacement(VECTOR_INDEX[1:number_of_vibrational_degrees+1], n+2) ]    
+    return [''.join(i) for i in combinations_with_replacement(VECTOR_INDEX[1:number_of_vibrational_degrees+1], n+2)]
 
 def VECTORS_m(n:int, vec, number_of_vibrational_degrees):
   '''генерирует список с возможными значениями k для вектора m_i=n_i+k_i, k_i принимает значение [-n:n] и сумма модулей k_i   меньше или равна n
@@ -21,7 +21,7 @@ def VECTORS_m(n:int, vec, number_of_vibrational_degrees):
  # rng = range(-n,n+1)
   el = [i for i in product(range(-n,n+1), repeat=number_of_vibrational_degrees) if sum(map(abs,i)) <=n]
   el = np.array(el) + vec
-  return list(map(tuple,el))
+  return list(map(tuple, el))
 
 def factor_k(str):
   return(2**(-len(str)/2))
@@ -32,28 +32,28 @@ def factor_fi(str):
 
 def dict_gen(n, omega, const, constType):
 
-  n_list={}
+  n_list = {}
   for i in range(len(n)):
     #n_list[sy.symbols('n_'+str(n[i]['letIndex']))]=int(n[i]['value'])
-    n_list[sy.symbols('n_'+str(n[i]['letIndex']))]=0
+    n_list[sy.symbols('n_'+str(n[i]['letIndex']))] = 0
 
-  omega_list={}
+  omega_list = {}
   for i in range(len(omega)):
-    omega_list[sy.symbols('omega_'+str(omega[i]['letIndex']))]=float(omega[i]['value'])
+    omega_list[sy.symbols('omega_'+str(omega[i]['letIndex']))] = float(omega[i]['value'])
 
-  const_changed=copy.deepcopy(const)
+  const_changed = copy.deepcopy(const)
   if constType=='k':
     list(map(lambda x: x.update(value=float(x['value'])*factor_k(x['letIndex'])),const_changed))
   elif constType=='fi':
     list(map(lambda x: x.update(value=float(x['value'])*factor_fi(x['letIndex'])),const_changed))
 
-  const_list_changed={}
+  const_list_changed = {}
   for i in range(len(const_changed)):
-    const_list_changed[sy.symbols('A_'+str(const_changed[i]['letIndex']))]=float(const_changed[i]['value'])
+    const_list_changed[sy.symbols('A_'+str(const_changed[i]['letIndex']))] = float(const_changed[i]['value'])
 
-  D_0=sy.symbols('D_0')
+  D_0 = sy.symbols('D_0')
 
-  d_list={D_0:0}
+  d_list = {D_0: 0}
 
   complete_dict = {**n_list, **omega_list, **const_list_changed, **d_list}
   
--- flask-server/index.py
+++ flask-server/index.py
@@ -1,8 +1,9 @@
 from itertools import combinations_with_replacement
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import sympy as sy
 
-from const_new import *
+# from const_new import *
 import constant_gen_new
 import Recurrence_Relations
 
@@ -21,11 +22,15 @@ def home():
 
 @app.route('/api/v1/calculation', methods=['GET', 'POST'])
 def get_test():
-    request_data = request.get_json(force = True)
+    request_data = request.get_json(force=True)
 
-    #constant_gen_new.constant_gen(request_data['numbers2'], request_data['omegas'], request_data['consts'], request_data['constsType'])
+    # constant_gen_new.constant_gen(request_data['numbers2'], request_data['omegas'], request_data['consts'], request_data['constsType'], request_data['order'])
 
     complete_dict = dict_gen(request_data['numbers2'], request_data['omegas'], request_data['consts'], request_data['constsType'])
+
+    n_dict = {}
+    for i in range(len(request_data['numbers2'])):
+        n_dict[sy.symbols('n_' + str(request_data['numbers2'][i]['letIndex']))] = 0
 
     n_list1 = [int(item['value']) for item in request_data['numbers1']]
     n_list2 = [int(item['value']) for item in request_data['numbers2']]
@@ -35,11 +40,11 @@ def get_test():
 
     constType = request_data['constsType']
 
-    #energy = Recurrence_Relations.AE_BD(n_list, n_list, 2)
-    energy = sum([Recurrence_Relations.AE_BD(n_list2, n_list2, i) for i in range(3)])
-    energy -= sum([Recurrence_Relations.AE_BD(n_list1, n_list1, i) for i in range(3)])
+    # energy = Recurrence_Relations.AE_BD(n_list, n_list, 2)
+    energy = sum([Recurrence_Relations.AE_BD(n_list2, n_list2, i, n_dict) for i in range(request_data['order'] + 1)])
+    energy -= sum([Recurrence_Relations.AE_BD(n_list1, n_list1, i, n_dict) for i in range(request_data['order'] + 1)])
 
-    response = jsonify(transition='%s > %s'%(n_str1, n_str2) , energy='%s'%(energy.subs(complete_dict)))
+    response = jsonify(transition='%s > %s' % (n_str1, n_str2), energy='%s' % (energy.subs(complete_dict)))
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
@@ -49,9 +54,10 @@ def config_response():
     freedomDegrees = request.args.get('freedomDegrees')
     order = request.args.get('order')
 
-    index_list = [str(i+1) for i in range(int(freedomDegrees))]
-    omegas_list = [{'index':i+1, 'value':''} for i in range(int(freedomDegrees))]
-    consts_list = sum([[{'index': ''.join(i), 'value':'', 'var': 'const'} for i in combinations_with_replacement(''.join(index_list), j+3)] for j in range(int(order))],[])
+    index_list = [str(i + 1) for i in range(int(freedomDegrees))]
+    omegas_list = [{'index': i + 1, 'value': ''} for i in range(int(freedomDegrees))]
+    consts_list = sum([[{'index': ''.join(i), 'value': '', 'var': 'const'} for i in
+                        combinations_with_replacement(''.join(index_list), j + 3)] for j in range(int(order))], [])
     data = {
         'omegas_list': omegas_list,
         'consts_list': consts_list
