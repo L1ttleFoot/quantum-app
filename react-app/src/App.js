6e8156027f722b9c032234d28f23da95854a0dfc
L1ttleFoot <leonid.klinck@yandex.ru>
2022-12-29 21:32:16 +0800

    disable dipole calculation because of timeout but add drawer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 import Main from './common'
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      <Router>
+        <Main/>
+      </Router>
     </div>
   );
 }
--- react-app/src/common/index.js
+++ react-app/src/common/index.js
@@ -1,288 +1,61 @@
 import React, {useState, useEffect} from 'react'
-import { AppBar, Toolbar, Button, TextField, FormControl, FormControlLabel, RadioGroup, Radio, IconButton, Tooltip } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import MainPage from "./screens/main"
+import { Routes, Route } from 'react-router-dom'
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, Divider } from '@mui/material';
 import './style.css'
-import UploadFileIcon from '@mui/icons-material/UploadFile';
+import StartPage from './screens/startPage'
+import CalculationPage from './screens/calculationPage'
+import { useNavigate } from "react-router";
+import ArticleIcon from '@mui/icons-material/Article';
+import CalculateIcon from '@mui/icons-material/Calculate';
 
 const Main = () => {
 
-    const dispatch = useDispatch()
-
-    const state = useSelector(state=>state)
-
-    const [omegasList, setOmegasList] = useState([])
-    const [constsList, setConstsList] = useState([])
-    const [dipoleXList, setDipoleXList] = useState([])
-    const [dipoleYList, setDipoleYList] = useState([])
-    const [dipoleZList, setDipoleZList] = useState([])
-
-    const [loading, setLoading] = useState(false)
-
-    const [fileName, setFileName] = useState('')
-    const [load, setLoad] = useState(false)
-
-    const setRows = (value) => {
-        dispatch({type:'SET_ROWS', payload: value})
-    }
-
-    const handleClickConfig = (props) => {
-
-        props.setLoading(true)
-
-        fetch(`https://quantum-app-bf8b.vercel.app/api/v1/config?freedomDegrees=${state.freedomDegrees}&order=${state.order}`)
-        .then(response=>response.json())
-        .then(data=>{
-            console.log(data)
-            setConstsList(data.consts_list)
-            setOmegasList(data.omegas_list)
-            setDipoleXList(data.dipole_list_x)
-            setDipoleYList(data.dipole_list_y)
-            setDipoleZList(data.dipole_list_z)
-        })
-
-        props.setLoading(false)
-
-    };
-
-    const handleClickCalculation = async() => {
-        setLoad(l=>!l)
-        await fetch(`https://quantum-app-bf8b.vercel.app/api/v1/calculation`,
-        {   method: 'POST',  
-            body: JSON.stringify({
-                numbers1: state.numbers1, 
-                numbers2: state.numbers2, 
-                omegas: state.omegas, 
-                consts: state.consts, 
-                dipoleX: [state.dipole0, ...state.dipoleX],
-                dipoleY: [state.dipole0, ...state.dipoleY],
-                dipoleZ: [state.dipole0, ...state.dipoleZ],
-                constsType: state.constsType,
-                order: state.order
-        })})
-        .then(response=>response.json())
-        .then(data=>{
-            setRows([...state.rows, data])
-        })
-        .catch(error => {
-            console.log(error)
-        });
-
-        setLoad(l=>!l)
-
-    };
-
-    const setConstsType = (e) => {
-        dispatch({type:'SET_CONSTS_TYPE', payload: e.target.value})
-    }
-
-    const setFreedomDegrees = (e) => {
-        dispatch({type:'SET_FREEDOM_DEGREES', payload: e.target.value})
-    }
-
-    const setOrder = (e) => {
-        dispatch({type:'SET_ORDER', payload: e.target.value})
-    }
-
-    const setOmegas = (value) => {
-        dispatch({type:'SET_OMEGAS', payload: value})
-    }
-
-    const setConsts = (value) => {
-        dispatch({type:'SET_CONSTS', payload: value})
-    }
-
-    const setDipoleX = (value) => {
-        dispatch({type:'SET_DIPOLE_X', payload: value})
-    }
-
-    const setDipoleY = (value) => {
-        dispatch({type:'SET_DIPOLE_Y', payload: value})
-    }
-
-    const setDipoleZ = (value) => {
-        dispatch({type:'SET_DIPOLE_Z', payload: value})
-    }
-
-    const emptyNumbers1 = [undefined, ''].some(item=>state.numbers1.map(el=>el.value).includes(item))
-    const emptyNumbers2 = [undefined, ''].some(item=>state.numbers2.map(el=>el.value).includes(item))
-    const emptyOmegas = [undefined, ''].some(item=>state.omegas.map(el=>el.value).includes(item))
-    const emptyConsts = [undefined, ''].some(item=>state.consts.map(el=>el.value).includes(item))
-
-    const someEmpty = emptyNumbers1 || emptyNumbers2 || emptyOmegas || emptyConsts
-
-    const letterIndexes='ijkl'
-
-    const numberIndex = {
-        'i':'1',
-        'j':'2',
-        'k':'3',
-        'f':'4',
-    }
-
-    const onChange = (e) => {
-        
-        let reader = new FileReader()
-
-        reader.onload = function(event) {
-
-            console.log(e.target.files[0].name)
-
-            setFileName(e.target.files[0].name)
-
-            let objFreedomDegrees = event.target.result.split('\n').filter(item=>item.includes('number_of_vibrational_degrees'))[0].trim().split('=')[1]
-
-            let objOrder = event.target.result.split('\n').filter(item=>item.includes('max_indignation'))[0].trim().split('=')[1]
-
-            let objConstType = event.target.result.split('\n').filter(item=>item.includes('type_anharmonic_const'))[0].trim().split('=')[1].replace(/["']/g,'')
-
-            let objOmegas = JSON.parse(event.target.result.split('\n').filter(item=>item.includes('const_omega'))[0].trim().split('=')[1].replace(/omega_[a-z]/g, x=> `"${x}"`))
-        
-            let objConsts = JSON.parse(event.target.result.split('\n').filter(item=>item.includes('const_anharmonic'))[0].trim().split('=')[1].replace(/A_[a-z]{0,4}/g, x=> `"${x}"`))
-
-            let objDipoleX = JSON.parse(event.target.result.split('\n').filter(item=>item.includes('const_dipoleX_dict'))[0].trim().split('=')[1].replace(/D_[0-9,a-z]{0,4}/g, x=> `"${x}"`))
-
-            let objDipoleY = JSON.parse(event.target.result.split('\n').filter(item=>item.includes('const_dipoleY_dict'))[0].trim().split('=')[1].replace(/D_[0-9,a-z]{0,4}/g, x=> `"${x}"`))
-
-            let objDipoleZ = JSON.parse(event.target.result.split('\n').filter(item=>item.includes('const_dipoleZ_dict'))[0].trim().split('=')[1].replace(/D_[0-9,a-z]{0,4}/g, x=> `"${x}"`))
-
-            dispatch({type:'SET_FREEDOM_DEGREES', payload: parseInt(objFreedomDegrees)})
-
-            dispatch({type:'SET_ORDER', payload: parseInt(objOrder)})
-
-            dispatch({type:'SET_CONSTS_TYPE', payload: objConstType})
-
-            setOmegas([...Object.values(objOmegas).map((item, index)=>({var: 'number', index: index+1, value: `${item}`, letIndex: letterIndexes[index]}))])
-
-            setConsts([...Object.entries(objConsts).map(item=>({var:'const', index: item[0].split('_')[1].replace(/[i,j,k,l]/g, x=>numberIndex[x]), value: `${item[1]}`, letIndex: item[0].split('_')[1]}))])
-
-            setDipoleX([...Object.entries(objDipoleX).map(item=>({var:'const', index: item[0].split('_')[1].replace(/[i,j,k,l]/g, x=>numberIndex[x]), value: `${item[1]}`, letIndex: item[0].split('_')[1]}))].slice(1))
-
-            setDipoleY([...Object.entries(objDipoleY).map(item=>({var:'const', index: item[0].split('_')[1].replace(/[i,j,k,l]/g, x=>numberIndex[x]), value: `${item[1]}`, letIndex: item[0].split('_')[1]}))].slice(1))
-
-            setDipoleZ([...Object.entries(objDipoleZ).map(item=>({var:'const', index: item[0].split('_')[1].replace(/[i,j,k,l]/g, x=>numberIndex[x]), value: `${item[1]}`, letIndex: item[0].split('_')[1]}))].slice(1))
-
-        }
-        
-        reader.readAsText(e.target.files[0])
-        //e.target.value = "";
-    }
-
-    const onClick = (e) => {
-        e.target.value = '';
-    }
-
-    useEffect(
-        () => {
-            setConstsList(state.consts)
+    const menuItems = [
+        {
+          text: 'Info',
+          icon: <ArticleIcon/>,
+          path: '/'
         },
-        [state.consts],
-    );
-
-    useEffect(
-        () => {
-            setDipoleXList(state.dipoleX)
+    
+        {
+          text: 'Calculation',
+          icon: <CalculateIcon/>,
+          path: '/calculation'
         },
-        [state.dipoleX],
-    );
+    ]
 
-    useEffect(
-        () => {
-            setDipoleYList(state.dipoleY)
-        },
-        [state.dipoleY],
-    );
-
-    useEffect(
-        () => {
-            setDipoleZList(state.dipoleZ)
-        },
-        [state.dipoleZ],
-    );
-
-    useEffect(
-        () => {
-            setOmegasList(state.omegas)
-        },
-        [state.omegas],
-    );
+    const navigate = useNavigate()
 
     return(
-        <div>
-            <AppBar style={{background: 'white', position:'absolute'}}>
-                <Toolbar style={{display: 'flex' }}>
-                    
-                        <div className='input'>
+        <div sx={{ display: 'flex' }}>
 
-                            <TextField value={state.freedomDegrees} style={{margin: 10, width: 130}} label={'Степени свободы'} size='small' onChange={setFreedomDegrees}></TextField>
+            <Drawer
+                style={{width:50}}
+                variant='permanent'
+                anchor='left'
+            >
+                <Toolbar />
+                <Divider />
 
-                            <TextField value={state.order} style={{margin: 10, width: 80}} label={'Порядок'} size='small' onChange={setOrder}></TextField>
+                <List>
+                {menuItems.map(item => (
+                    <ListItem
+                        button
+                        key={item.text}
+                        onClick={() => navigate(item.path)}
+                    >
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text}/>
+                    </ListItem>
+                ))}
+                </List>
 
-                            <div>
-                                <div className='radio-lable'>Тип констант</div>
-                                <FormControl style={{marginLeft: 10, padding: '0 10px', border: '1px solid #c4c4c4', borderRadius: 4,}}>
-                                    <RadioGroup
-                                        row
-                                        value={state.constsType}
-                                        onChange={(e)=>setConstsType(e)}
-                                    >
-                                        <FormControlLabel style={{color:'#0E76BB'}} value="A" control={<Radio size='small' />} label="a"/>
-                                        <FormControlLabel style={{color:'#0E76BB'}} value="k" control={<Radio size='small' />} label="k" />
-                                        <FormControlLabel style={{color:'#0E76BB'}} value="fi" control={<Radio size='small' />} label={<span>&phi;</span>}  />
-                                    </RadioGroup>
-                                </FormControl> 
-                            </div>
-                        </div>
+            </Drawer>
 
-                        <div className='buttons'>
-
-                            <Button 
-                                onClick={()=>handleClickConfig({setLoading})} 
-                                variant={"outlined"}
-                                style={{background: 'white', margin: 10}}
-                                disabled={loading}
-                            >
-                                Параметры
-                            </Button>
-
-                            <Tooltip title="Загрузить файл" placement="right">
-                                <IconButton onClick={onClick} color="primary" component="label">
-                                    <input
-                                        onChange={onChange}
-                                        
-                                        accept="application/json,.py,.txt"
-                                        type="file"
-                                        hidden
-                                    />
-                                    <UploadFileIcon />
-                                </IconButton>
-                            </Tooltip>
-
-                           {/*  <Button 
-                                disabled={emptyNumbers1 || emptyNumbers2 || emptyOmegas || emptyConsts} 
-                                onClick={handleClickCalculation} 
-                                variant="outlined" 
-                                style={{background: 'white', margin: 10}}
-                            >
-                                Расчет
-                            </Button> */}
-                            
-                        </div>
-
-                </Toolbar>
-            </AppBar>
-
-            <MainPage 
-                state = {state} 
-                frequencyList = {omegasList} 
-                constsList = {constsList}
-                dipoleXList = {dipoleXList}
-                dipoleYList = {dipoleYList}
-                dipoleZList = {dipoleZList}
-                calculation = {handleClickCalculation}
-                someEmpty = {someEmpty}
-                load = {load}
-            />
+            <Routes>
+                    <Route path='/' element={<StartPage/>}></Route>
+                    <Route path='/calculation' element={<CalculationPage/>}></Route>
+            </Routes>
 
         </div>
     )
