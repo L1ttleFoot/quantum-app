3562320ad15d5728013d98e96da11484a597ceeb
19810313 <lbklink@sberbank.ru>
2023-01-12 12:38:23 +0800

    edit tab menu

--- react-app/src/common/index.js
+++ react-app/src/common/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { Routes, Route } from 'react-router-dom'
+import {Routes, Route, useLocation} from 'react-router-dom'
 import { Drawer, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Divider } from '@mui/material';
 import './style.css'
 import StartPage from './screens/startPage'
@@ -11,6 +11,20 @@ import CalculateIcon from '@mui/icons-material/Calculate';
 const Main = () => {
 
     const [tab, setTab] = useState('Info')
+
+    const location = useLocation();
+
+    useEffect(() => {
+
+        if (location.pathname.indexOf('calculation') > 0) {
+            setTab('Calculation');
+        }
+
+        else {
+            setTab('Info');
+        }
+
+    }, [location]);
 
     const menuItems = [
         {
